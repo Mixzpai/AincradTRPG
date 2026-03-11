607838551111e9258ac3eb21b3630568e9add774
Mikka <166986251+Mixzpai@users.noreply.github.com>
2026-03-11 19:58:00 +0800

    Add files via upload

---
+++ SAOTRPG/BossMonsters.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class BossMonster : Monster
+{
+    public BossMonster()
+    {
+        Id = 7;
+        Name = "Illfang the Kobald Lord";
+        Level = 15;
+        Vitality = 8;
+        Strength = 6;
+        Endurance = 5;
+        Dexterity = 4;
+        Agility = 3;
+        Intelligence = 2;
+        BaseAttack = 10;
+        BaseCriticalRate = 5;
+        BaseCriticalHitDamage = 10;
+        BaseDefense = 8;
+        BaseSpeed = 6;
+        BaseSkillDamage = 4;
+        ExperienceYield = 1000;
+        ColYield = 5000;
+        CurrentHealth = MaxHealth;
+    }
+}
---
+++ SAOTRPG/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+
+namespace REDACTED_PROJECT_NAME
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Test Player Creation
+            Player PlayerInstance = new Player();
+            PlayerInstance = Player.CreateNewPlayer();
+
+            Console.WriteLine();
+            PlayerInstance.DisplayPlayerStats();
+
+            Console.WriteLine();
+            PlayerInstance.SpendSkillPoints();
+
+            Console.WriteLine();
+            PlayerInstance.DisplayPlayerStats();
+
+            Console.WriteLine();
+
+
+            // Test monster
+            Console.WriteLine();
+            BossMonster illfang = new BossMonster();
+
+            Console.WriteLine($"<<{illfang.Name}>> Encountered!");
+            Console.WriteLine($"| LVL:{illfang.Level} | HP:({illfang.MaxHealth}/{illfang.CurrentHealth}) | " +
+                $"STR:{illfang.Strength} | END:{illfang.Endurance} | DEX:{illfang.Dexterity} | AGI:{illfang.Agility} | INT:{illfang.Intelligence} |");
+            Console.WriteLine();
+
+            while (!illfang.IsDefeated)
+            {
+                int damage = PlayerInstance.AttackMonster(illfang);
+                Console.WriteLine($"{PlayerInstance.FirstName} attacked {illfang.Name} with {damage} damage...");
+                Console.WriteLine();
+
+                var reward = illfang.TakeDamage(damage);
+                Console.WriteLine();
+                
+                if (illfang.IsDefeated && reward != null)
+                {
+                    PlayerInstance.GainExperience(reward.Experience);
+                    break;
+                }
+            }
+        }
+    }
+}
